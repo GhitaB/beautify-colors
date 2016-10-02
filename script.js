b41f5cc33cac931402784ddd87f2bbe3899c7a44
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-10-02 14:20:41 +0300

    More on behaviour.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  alert("Hello.");
+  function set_background_color() {
+    var $this_box = $(this).parent();
+    var new_color = "#" + this.value;
+
+    $this_box.css('background-color', new_color);
+  };
+
+  function beautify() {
+    alert("Starting...");
+  };
+
+  $("input.jscolor").on("change", set_background_color);
+
+  $("button#beautify").on("click", beautify);
 });
