60f37ffa4682b82e6c6e8703ef91a227fe87f3d4
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-10-02 14:50:39 +0300

    Beautify.

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ $(document).ready(function() {
   };
 
   function beautify() {
-    alert("Starting...");
+    var color_1 = "#" + $("#color-original-1").val();
+    var color_2 = "#" + $("#color-original-2").val();
+    var color_3 = "#" + $("#color-original-3").val();
+    var color_4 = "#" + $("#color-original-4").val();
+    var color_5 = "#" + $("#color-original-5").val();
+
+    $("#color-final-1").css('background-color', color_1);
+    $("#color-final-2").css('background-color', color_2);
+    $("#color-final-3").css('background-color', color_3);
+    $("#color-final-4").css('background-color', color_4);
+    $("#color-final-5").css('background-color', color_5);
   };
 
   $("input.jscolor").on("change", set_background_color);
