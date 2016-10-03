97947b364e5252974dce5ba163e36645b767aa03
Ghiță Bizău <ghita_bizau@yahoo.com>
2016-10-03 06:53:51 +0300

    HSL to RGB.

--- script.js
+++ script.js
@@ -93,6 +93,32 @@ $(document).ready(function() {
   };
 
 
+  function hsl_to_rgb(h, s, l){
+      var r, g, b;
+
+      if(s == 0){
+          r = g = b = l; // achromatic
+      }else{
+          var hue2rgb = function hue2rgb(p, q, t){
+              if(t < 0) t += 1;
+              if(t > 1) t -= 1;
+              if(t < 1/6) return p + (q - p) * 6 * t;
+              if(t < 1/2) return q;
+              if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+              return p;
+          }
+
+          var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+          var p = 2 * l - q;
+          r = hue2rgb(p, q, h + 1/3);
+          g = hue2rgb(p, q, h);
+          b = hue2rgb(p, q, h - 1/3);
+      }
+
+      return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  }
+
+
   function get_max_saturation() {
     // return saturation of primary color
     var max_saturation = 0;
@@ -137,7 +163,14 @@ $(document).ready(function() {
 
     var max_saturation = get_max_saturation();
 
+    var rgb_beautified_color_1 = hsl_to_rgb(hsl_color_1[0], hsl_color_1[1], hsl_color_1[2]);
+    var rgb_beautified_color_2 = hsl_to_rgb(hsl_color_2[0], hsl_color_2[1], hsl_color_2[2]);
+    var rgb_beautified_color_3 = hsl_to_rgb(hsl_color_3[0], hsl_color_3[1], hsl_color_3[2]);
+    var rgb_beautified_color_4 = hsl_to_rgb(hsl_color_3[0], hsl_color_3[1], hsl_color_3[2]);
+    var rgb_beautified_color_5 = hsl_to_rgb(hsl_color_3[0], hsl_color_3[1], hsl_color_3[2]);
+
     // TODO: continue here
+    // rgb to hex
     // beautify colors saturation
 
     $("#color-final-1").css('background-color', data.original.color_1.value);
